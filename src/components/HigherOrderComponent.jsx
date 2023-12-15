f30cffd86191a3ec75965f95343e561461abc1d2
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-12-15 12:29:26 +0530

    first commit

--- src/components/HigherOrderComponent.jsx
+++ src/components/HigherOrderComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './HigherOrderComponent.css';
+import "./HigherOrderComponent.css"
 
 const HigherOrderComponent = () => {
   const userData = [
@@ -10,8 +10,8 @@ const HigherOrderComponent = () => {
     { id: '5', name: 'Jack', user_type: 'Designer', age: 43, years: 18 }
   ];
 
-  const renderItems = (items) =>
-    items.map((item) => (
+  const renderItems = () => {
+    return userData.map((item) => (
       <React.Fragment key={item.id}>
         <ul className="list-elements">
           <span>Id: {item.id}</span>
@@ -20,38 +20,82 @@ const HigherOrderComponent = () => {
         </ul>
       </React.Fragment>
     ));
+  };
 
-  const filteredUsers2 = userData.filter((user) => user.user_type === 'Designer');
-  const filteredUsers3 = userData.filter((user) => user.name.startsWith('J'));
-  const filteredUsers4 = userData.filter((user) => user.age > 30);
-  const totalYearsOfDesigners = filteredUsers2.reduce((sum, designer) => sum + designer.years, 0);
+  const filteredUsers = userData.filter((user) => user.user_type === 'Designer');
+
+  const renderItems2 = () => {
+    return filteredUsers.map((item) => (
+      <React.Fragment key={item.id}>
+        <ul className="list-elements">
+          <span>Id: {item.id}</span>
+          <span>Name: {item.name}</span>
+          <span>User Type: {item.user_type}</span>
+        </ul>
+      </React.Fragment>
+    ));
+  };
+
+  const filteredUsers2 = userData.filter((user) => user.name.startsWith('J')); 
+
+  const renderItem3 =() =>{
+    return filteredUsers2.map((item) => (
+        <React.Fragment key={item.id}>
+            <ul className="list-elements">
+            <span>Id: {item.id}</span>
+            <span>Name: {item.name}</span>
+            <span>User Type: {item.user_type}</span>
+            </ul>
+        </React.Fragment>
+    ))
+  }
+
+  const filteredUsers3 = userData.filter((user) => user.age > 30); 
+
+  const renderItem4 =() =>{
+    return filteredUsers3.map((item) => (
+        <React.Fragment key={item.id}>
+            <ul className="list-elements">
+            <span>Id: {item.id}</span>
+            <span>Name: {item.name}</span>
+            <span>User Type: {item.user_type}</span>
+            </ul>
+        </React.Fragment>
+    ))
+  }
+  const designers = userData.filter((user) => user.user_type === 'Designer');
+  const filteredUsers4 = designers.reduce((sum, designer) => sum + designer.years, 0);
+
 
   return (
     <div>
-      <h>Display all items</h>
-      <div className="table-1">
-        <ul>{renderItems(userData)}</ul>
-      </div>
+        <h>Display all itmes</h>
+        <div className='table-1' >
+      <ul>{renderItems()}</ul>
+    </div>
 
-      <h>Display based on data type</h>
-      <div className="table-2">
-        <ul>{renderItems(filteredUsers2)}</ul>
-      </div>
+        <h>Display based on data type</h>
+    <div className='table-2'>
+      <ul>{renderItems2()}</ul>
+    </div>
 
-      <h>Filter all data starting with J</h>
-      <div className="table-3">
-        <ul>{renderItems(filteredUsers3)}</ul>
-      </div>
+    <h>Filter all data starting with J</h>
+    <div className='table-3'>
+        
+        <ul>{renderItem3()}</ul>
+    </div>
 
-      <h>Filter the data based on age</h>
-      <div className="table-4">
-        <ul>{renderItems(filteredUsers4)}</ul>
-      </div>
+    <h> Filter the data based on age</h>
+    <div className='table-4'>
+        <ul>{renderItem4()}</ul>
+    </div>
 
-      <h>Total Years of Experience of Designers</h>
-      <div className="table-5">
-        <ul>{totalYearsOfDesigners}</ul>
-      </div>
+    
+    <h>Total Years of Experience of Designers</h>
+    <div className='table-5'>
+      <ul>{filteredUsers4}</ul>
+    </div>
+
     </div>
   );
 };
